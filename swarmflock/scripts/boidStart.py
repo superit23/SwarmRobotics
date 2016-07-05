8742efbb2dd3e05163146de3bb8c465793c68847
Dan Cronce <dcfriendsms@gmail.com>
2016-07-05 10:50:52 -0500

    Adding test scripts.

--- swarmflock/scripts/boidStart.py
+++ swarmflock/scripts/boidStart.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 import boid
 import numpy as np
+import os
+import rospy
 
-print "Hello"
 maxSpeed = 4
 maxForce = 0.5
 desiredSep = 100
@@ -11,15 +12,27 @@ sepWeight = 10
 alignWeight = 0.1
 cohWeight = 0.2
 
-boids = []
+rospy.set_param("/boids/maxSpeed", str(maxSpeed))
+rospy.set_param("/boids/maxForce", str(maxForce))
+rospy.set_param("/boids/desiredSep",  str(desiredSep))
+rospy.set_param("/boids/neighborRadius", str(neighborRadius))
+rospy.set_param("/boids/sepWeight", str(sepWeight))
+rospy.set_param("/boids/alignWeight", str(alignWeight))
+rospy.set_param("/boids/cohWeight", str(cohWeight))
 
-for i in range(40):
+for i in range(1, sys.argv):
   location = np.random.uniform(-250, 250, size=(1,2))
-  boids.append(boid.Boid(location, maxSpeed, maxForce, desiredSep, neighborRadius, sepWeight, alignWeight, cohWeight))
+  os.system("./nRobo.py " + sys.argv[i], location)
 
-for i in range(100):
-  for boid in boids:
-    boid.step(boids)
+#boids = []
 
-for boid in boids:
-  print boid.location
+#for i in range(40):
+#  location = np.random.uniform(-250, 250, size=(1,2))
+#  boids.append(boid.Boid(location, maxSpeed, maxForce, desiredSep, neighborRadius, sepWeight, alignWeight, cohWeight))
+
+#for i in range(100):
+#  for boid in boids:
+#    boid.step(boids)
+
+#for boid in boids:
+#  print boid.location
